e436c00440e1dec49992e8d371289ea15b2fa9c9
hersheyHarish <sundara.harish@gmail.com>
2023-09-09 21:59:13 -0700

    first commit

---
+++ components/Head.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Head(){
+    return(
+        <header className="header">
+            <h2 className="header--title">MemeGen</h2>
+            <h3 className="header--project">Project 3 - React </h3>
+        </header>
+    )
+}
---
+++ components/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "./Head";
+import Meme from "./Meme";
+
+export default function App(){
+    return(
+        <div>
+            <Head/> 
+            <Meme/>       
+        </div>
+       
+    )
+}
